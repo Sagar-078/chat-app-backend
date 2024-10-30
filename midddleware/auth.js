44955b0d4a3ef44f058c550da95b6918ead2a42f
Sagar-078 <139355166+Sagar-078@users.noreply.github.com>
2024-10-30 16:19:57 +0530

    Update auth.js

--- midddleware/auth.js
+++ midddleware/auth.js
@@ -9,9 +9,6 @@ exports.auth = async(req, res, next) => {
     const token = req.cookies.token || req.body.token                           
                     || req.header("Authorization").replace("Bearer ", "");
 
-  
-
-    // if token is missing
     if(!token){
       return res.status(401).json({
         success: false,
@@ -19,10 +16,8 @@ exports.auth = async(req, res, next) => {
       })
     }
 
-    // else verify token
     try{
 
-      // decode json web  token
       const decode = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = decode
